9b50f992da3909b1f1c74193d97a0e78c4c316d8
MiguelCoba0502 <miguel_8090@hotmail.com>
2024-10-23 21:54:26 -0600

    Initial Commit

---
+++ Prueva Clima/Prueva_ClimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prueva_ClimaApp.swift
+//  Prueva Clima
+//
+//  Created by Eduardo Coba on 23/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Prueva_ClimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
